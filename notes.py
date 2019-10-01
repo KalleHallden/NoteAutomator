39220089f851caa44a4fde190fd2274f5cdbee78
kalle hålldén <kallehalldeen@gmail.com>
2019-10-01 13:22:23 +0200

    Adds the notes in the right folders

--- notes.py
+++ notes.py
@@ -2,10 +2,43 @@
 import sys
 import os 
 
+python = ".py"
+java = ".java"
+dart = ".dart"
+text = ".txt"
+
+extensions = {
+    "python" : python,
+    ".py" : python,
+    "text" : text,
+    ".txt" : text,
+    "java" : java,
+    ".java" : java,
+    "dart" : dart,
+    "flutter" : dart,
+    ".dart" : dart
+}
+
 def create():
-    print(sys.argv)
-    fileName = str(sys.argv[1]) + str(sys.argv[3])
-    open(fileName, "a").close()
+    extension = str(sys.argv[3])
+    folderName = str(sys.argv[2])
+    fileName = str(sys.argv[1])
+    os.chdir("./Notes")
+    try:
+        extension = extensions[extension]
+    except Exception:
+        extension = ".txt"
+
+    fileName = fileName + extension
+    if os.path.isdir("./" + folderName):
+        os.chdir("./" + folderName)
+    else:
+        os.mkdir(folderName)
+        os.chdir("./" + folderName)
+    
+    if not os.path.isfile("./" + fileName):
+        open(fileName, "a").close()
+
     os.system("subl " + fileName)
 
 if __name__ == "__main__":
